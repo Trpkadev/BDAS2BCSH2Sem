34774f55d31bfa339c79212285271db52e45f79c
Trpkadev <trpkasteam@gmail.com>
2024-11-02 13:20:37 +0100

    dokumentace

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,12 +1,10 @@
-﻿namespace BCSH2BDAS2.Models
+﻿namespace BCSH2BDAS2.Models;
+
+public enum Role
 {
-    public enum Role
-    {
-        User = 1,
-        Maintainer = 2,
-        Dispatcher = 3,
-        Manager = 4,
-        Admin = 5,
-        Owner = 6
-    }
+    User = 1,
+    Maintainer = 2,
+    Dispatcher = 3,
+    Admin = 4,
+    Owner = 5
 }
